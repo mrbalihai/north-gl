44f3fa58e891269d8adeafa3f5a752d611ef8026
Rob Bollons <rob@robbollons.com>
2020-02-12 07:19:37 +0000

    Added more typing and normalizing structures

--- src/camera.ts
+++ src/camera.ts
@@ -1,38 +1,44 @@
-import gl = require('gl-matrix');
-import REGL = require('regl');
-const mat4 = gl.mat4;
+import { mat4 } from 'gl-matrix';
+import { Regl, DefaultContext } from 'regl';
 
 interface Props {
     eye: number[];
     target: number[];
 }
 
-interface Context extends REGL.DefaultContext {
-    view: gl.mat4;
-    projection: gl.mat4;
+interface Context extends DefaultContext {
+    view: mat4;
+    invView: mat4;
+    projection: number[];
 }
 
-export = (regl: REGL.Regl, props: Props) => regl<{}, REGL.Uniforms>({
-    context: {
-        projection: (context: REGL.DefaultContext) =>
-            mat4.perspective(mat4.create(),
-                Math.PI / 4,
-                context.viewportWidth / context.viewportHeight,
-                0.01,
-                1000.0),
+interface Uniforms {
+    view: mat4;
+    invView: mat4;
+    projection: number[];
+}
 
-        view: ({}) =>
-            mat4.lookAt(mat4.create(),
-                props.eye,
-                props.target,
-                [0, 1, 0]),
+export const createCamera = (regl: Regl, props: Props) =>
+    regl<Uniforms, {}, Props>({
+        context: {
+            projection: (context: Context) =>
+                mat4.perspective(mat4.create(),
+                    Math.PI / 4, // FOV
+                    context.viewportWidth / context.viewportHeight, // Viewport
+                    0.01, // Near
+                    1000.0), // Far
 
-        eye: regl.prop<Props, 'eye'>('eye')
-    },
+            view: ({}) =>
+                mat4.lookAt(mat4.create(),
+                    props.eye,
+                    props.target,
+                    [0, 1, 0]),
+            eye: regl.prop<Props, 'eye'>('eye')
+        },
 
-    uniforms: {
-        view: regl.context<Context, 'view'>('view'),
-        invView: (context: Context) => mat4.invert(mat4.create(), context.view),
-        projection: regl.context<Context, 'projection'>('projection')
-    }
-});
+        uniforms: {
+            view: regl.context<Context, 'view'>('view'),
+            invView: (context: Context): mat4 => mat4.invert(mat4.create(), context.view),
+            projection: regl.context<Context, 'projection'>('projection')
+        }
+    });
