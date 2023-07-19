8a93c5dd953fec367171033be019eb924db8d43b
Rob Bollons <rbollons@gmail.com>
2023-07-19 13:07:40 +0100

    Factor sky in the reflection and make reflection dynamic

--- src/camera.ts
+++ src/camera.ts
@@ -2,39 +2,39 @@ import { mat4, vec3 } from 'gl-matrix';
 import { Regl, DefaultContext } from 'regl';
 
 interface Props {
-    eye: vec3;
-    target: vec3;
+  eye: vec3;
+  target: vec3;
 }
 
 interface Uniforms {
-    view: mat4;
-    invertedView: mat4;
-    projection: mat4;
+  view: mat4;
+  invertedView: mat4;
+  projection: mat4;
 }
 
 type Context = Uniforms & DefaultContext;
 
 export const createCamera = (regl: Regl, props: Props) =>
-    regl<Uniforms, {}, Props>({
-        context: {
-            projection: (context: DefaultContext) =>
-                mat4.perspective(mat4.create(),
-                    Math.PI / 4, // FOV
-                    context.viewportWidth / context.viewportHeight, // Viewport
-                    0.01, // Near
-                    1000.0), // Far
+  regl<Uniforms, {}, Props>({
+    context: {
+      projection: (context: DefaultContext) =>
+        mat4.perspective(mat4.create(),
+          Math.PI / 4, // FOV
+          context.viewportWidth / context.viewportHeight, // Viewport
+          0.01, // Near
+          1000.0), // Far
 
-            view: ({}) =>
-                mat4.lookAt(mat4.create(),
-                    props.eye,
-                    props.target,
-                    [0, 1, 0]),
-        },
+      view: ({}) =>
+        mat4.lookAt(mat4.create(),
+          props.eye,
+          props.target,
+          [0, 1, 0]),
+    },
 
-        uniforms: {
-            view: regl.context<Context, 'view'>('view'),
-            invertedView: (context: Context): mat4 =>
-                mat4.invert(mat4.create(), context.view),
-            projection: regl.context<Context, 'projection'>('projection')
-        }
-    });
+    uniforms: {
+      view: regl.context<Context, 'view'>('view'),
+      invertedView: (context: Context): mat4 =>
+        mat4.invert(mat4.create(), context.view),
+      projection: regl.context<Context, 'projection'>('projection')
+    }
+  });
--- src/renderables/item/item.frag.glsl
+++ src/renderables/item/item.frag.glsl
@@ -6,13 +6,21 @@ precision mediump float;
 
 varying vec3 fragNormal, fragPosition;
 uniform vec3 eye;
+uniform vec3 skyColour;
 uniform DirectionalLight directionalLight;
 uniform Material material;
-
 
 void main() {
     vec3 norm = normalize(fragNormal);
     vec3 viewDirection = normalize(eye - fragPosition);
     vec3 light = calcDirectionalLight(directionalLight, material, norm, viewDirection);
-    gl_FragColor = vec4(light, 1.0);
+
+    // Calculate the sky colour contribution
+    vec3 skyColourContribution = skyColour * pow(max(dot(norm, viewDirection), 0.0), 2.0);
+
+    // Mix the sky colour contribution with the directional light contribution
+    vec3 finalColour = mix(skyColourContribution, light, 0.7);
+
+    gl_FragColor = vec4(finalColour, 1.0);
+
 }
